ab5f4aa538927e0e6f7d91d889862ed9f0d3eed7
atman-33 <gpbjk0304@gmail.com>
2025-05-07 08:28:38 +0900

    fix: update fs and path imports to use node: prefix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import path from 'path';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import fs from 'node:fs';
+import path from 'node:path';
 import puppeteer from 'puppeteer';
 import { launchChrome } from './lib/launch-chrome';
 import { waitForChrome } from './lib/wait-for-chrome';
